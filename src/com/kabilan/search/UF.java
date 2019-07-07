b44f4030366e9f483d0cc269891206c9f22ac67f
Kabilan T <kabilat@amazon.com>
2019-07-08 01:48:09 +0530

    implemented constructor of UF

--- src/com/kabilan/search/UF.java
+++ src/com/kabilan/search/UF.java
@@ -1,7 +1,13 @@
 package com.kabilan.search;
 
 public class UF {
+	private int[] id;
+
 	public UF(int n) {
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
 	}
 
 	public boolean connected(int p, int q) {
