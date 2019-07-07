5d303ac9775e8da7b36e6a2f2e7b86b0d7524762
Kabilan T <kabilat@amazon.com>
2019-07-08 01:57:00 +0530

    implemented connected and union operations of UF

--- src/com/kabilan/search/UF.java
+++ src/com/kabilan/search/UF.java
@@ -3,7 +3,7 @@ package com.kabilan.search;
 public class UF {
 	private int[] id;
 
-	public UF(int n) {
+	public UF(int N) {
 		id = new int[N];
 		for (int i = 0; i < N; i++) {
 			id[i] = i;
@@ -11,11 +11,21 @@ public class UF {
 	}
 
 	public boolean connected(int p, int q) {
-		//TODO fill logic
+		if(id[p] == id[q]) {
+			return true;
+		}
 		return false;
 	}
 
 	public void union(int p, int q) {
-		//TODO fill logic
+		// iterate the entire list and change all entries with value at index p
+		// to value at index q
+		int targetValue = id[q];
+		int sourceValue = id[p];
+		for (int i = 0; i < id.length; i++) {
+			if (id[i] == sourceValue) {
+				id[i] = targetValue;
+			}
+		}
 	}
 }
