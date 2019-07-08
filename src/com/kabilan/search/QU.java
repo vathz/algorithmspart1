c2809ba36f40958e02050f15f6214e2a75977e8b
Srivathsan <vathzsri@gmail.com>
2019-07-08 23:28:15 +0530

    Update QU.java

--- src/com/kabilan/search/QU.java
+++ src/com/kabilan/search/QU.java
@@ -2,11 +2,14 @@ package com.kabilan.search;
 
 public class QU {
 	private int[] id;
+	private int[] size[];
 
 	public QU(int N) {
 		id = new int[N];
+		size = new int[N];
 		for (int i = 0; i < N; i++) {
 			id[i] = i;
+			size[i] = 1;
 		}
 	}
 
@@ -17,14 +20,23 @@ public class QU {
 	public void union(int p, int q) {
 		int i = findRoot(p);
 		int j = findRoot(q);
-		id[i] = j;
+		if (size[i]<=size[j]) {
+			id[i] = j;
+			size[j]+=size[i];
+		}
+		else {
+			id[j] = i;
+			size[i]+=size[j];
+		}
 	}
 
 	private int findRoot(int p) {
 		// returns the root of the corresponding element
-		while(id[p] != p) {
-			p = id[p];
+		if (id[p] == p) {
+			return p;
 		}
-		return p;
+		int k = findRoot(id[p]);
+		id[p] = k;
+		return k;
 	}
 }
